829fd1ecb1d2e49ee77756868ce05ffae49729e2
Gyan Hajong <hazgyan@gmail.com>
2018-01-13 09:18:18 +0530

    Feeding input to the network

--- ann/ANN.java
+++ ann/ANN.java
@@ -5,9 +5,24 @@ import java.io.IOException;
 
 public class ANN {
 
+	int getTarget(int[] inputs) {
+		if(inputs[0]==1&&inputs[1]==1)
+			return 1;
+		else
+			return 0;
+	}
+
     public static void main(String[] args) throws IOException {
        NetworkBuilder NN = new NetworkBuilder({2,1});
-       int[] inputs;
+       int[][] inputs = new int[3][2];
+       inputs[0] = {0, 0};
+       inputs[1] = {0, 1};
+       inputs[1] = {1, 1};
+
+       for(int[] inputs: inputs) {
+
+       		learning(inputs, getTarget(inputs));
+       }
     }
     
 }
